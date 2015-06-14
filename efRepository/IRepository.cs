fab3b2ee440c9f9448e065e1eda1f65887fd0f8f
Ivan <ASladnev@gmail.com>
2015-06-14 13:41:43 +0300

    repository

---
+++ efRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using efModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efRepository
+{
+    public interface IRepository
+    {
+        Computer getComputerById(string computerId);
+    }
+}
---
+++ efRepository/Repository.cs
@@ -0,0 +1,23 @@
+﻿using efContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efRepository
+{
+    public class Repository : IRepository
+    {
+        private IContext _context;
+        public Repository (IContext context)
+        {
+            _context = context;
+        }
+
+        public efModel.Computer getComputerById(string computerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
