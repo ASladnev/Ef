24a95ecd441c30bb62bafaca3a913239c9e45074
Ivan <ASladnev@gmail.com>
2015-06-14 16:42:47 +0300

    repository

--- efRepositoryUnitTest/FakeContext.cs
+++ efRepositoryUnitTest/FakeContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using efContext;
+using efModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace efRepositoryUnitTest
 {
-    internal class FakeContext
+    internal class FakeContext : IContext
     {
+        public DbSet<Computer> Computer { get; set; }
+        public DbSet<Printer> Printer { get; set; }
+
     }
 }
